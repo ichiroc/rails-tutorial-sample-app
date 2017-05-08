66ffacc5e0aadfa8fb960080cd5be27bc7952a0b
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-05-08 18:53:50 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,9 +13,9 @@ module SessionsHelper
 
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if (user_id = session[:user_id])
+    if (user_id = session[:user_id]) # 明示的にログインした場合
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
+    elsif (user_id = cookies.signed[:user_id]) # remember me でログインする場合
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
