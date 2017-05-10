1ce84e54c0e40912edc631d7a03ff18b55fb0ec9
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-05-11 05:53:41 +0900

    Fix heroku app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   # heroku mail settings
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'ichiroc-rt-sample-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
